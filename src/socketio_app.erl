6a2e391fc0f14f1194db3b9db02a45b74c79c1e6
Yurii Rashkovskii <yrashk@gmail.com>
2011-02-15 15:30:56 -0800

    Application skeleton with simple_bridge dependency

---
+++ src/socketio_app.erl
@@ -0,0 +1,16 @@
+-module(socketio_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    socketio_sup:start_link().
+
+stop(_State) ->
+    ok.
