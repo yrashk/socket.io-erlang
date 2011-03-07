839793646faeb735ebd112e2a1b9484fc891f98c
Fred Hebert <mononcqc@gmail.com>
2011-03-07 13:11:44 -0500

    Drop support for stream-decoding.
    The complexity is so far unwarranted and can be handled by
    buffering the data on input.

--- src/socketio_data.erl
+++ src/socketio_data.erl
@@ -34,63 +34,26 @@ decode(#msg{content=Str}) when is_list(Str) ->
     header(Str).
 
 header(?FRAME ++ Rest) ->
-    header(Rest, []);
-header(L = [$~|_]) ->
-    {incomplete, fun(S) -> header(L++S) end}.
+    header(Rest, []).
 
 header(?FRAME ++ Rest=[_|_], Acc)->
     Length = list_to_integer(lists:reverse(Acc)),
     body(Length, Rest);
 header([N|Rest], Acc) when N >= $0, N =< $9 ->
-    header(Rest, [N|Acc]);
-header(L, Acc) when L =:= []; L =:= "~"; L =:= "~m" -> %% Breaking them macros
-    {incomplete, fun(S) -> header(L ++ S, Acc) end}.
+    header(Rest, [N|Acc]).
 
 body(Length, ?JSON_FRAME++Body) ->
     json(Length-3, Body);
 body(Length, ?HEARTBEAT_FRAME++Body) ->
     heartbeat(Length-3, Body, []);
-%% The 3 following clause avoid matching JSON or Heartbeats as messages
-%% In the case of streaming.
-body(Length, []) when Length > 3 ->
-    {incomplete, fun(S) -> body(Length, S) end};
-body(Length, L=[_]) when Length > 3 ->
-    {incomplete, fun(S) -> body(Length, L++S) end};
-body(Length, L=[_,_]) when Length > 3 ->
-    {incomplete, fun(S) -> body(Length, L++S) end};
 body(Length, Body) when length(Body) >= Length ->
-    #msg{content=lists:sublist(Body, Length)};
-body(Length, Body) ->  %% Return a continuation
-    LengthRemaining = Length - length(Body),
-    {incomplete,
-     fun(Input) -> body_stream(LengthRemaining, Input, Body) end}.
-
-body_stream(Length, Input, PartialBody) ->
-    NewPartial = lists:sublist(Input, Length),
-    Body = [PartialBody, NewPartial],
-    case Length - length(NewPartial) of
-        0 -> #msg{content=lists:flatten(Body)};
-        N when is_integer(N), N > 0 ->
-            {incomplete,
-             fun(NewInput) -> body_stream(N, NewInput, Body) end}
-    end.
+    #msg{content=lists:sublist(Body, Length)}.
 
 json(Length, Body) when length(Body) >= Length ->
     Object = lists:sublist(Body, Length),
-    #msg{content=jsx:json_to_term(list_to_binary(Object)), json=true};
-%% This stops caring about the content lenght. Maybe we should care about it,
-%% by mixing in the protocol and json stuff
-json(_Length, Body) ->
-    wrap(jsx:json_to_term(list_to_binary(Body), [{stream,true}])).
-
-wrap({incomplete, F}) ->
-    {incomplete, fun(Txt) -> wrap(F(list_to_binary(Txt))) end};
-wrap(X) ->
-    #msg{content=X, json=true}.
+    #msg{content=jsx:json_to_term(list_to_binary(Object)), json=true}.
 
 heartbeat(0, _, Acc) -> #heartbeat{index=list_to_integer(lists:reverse(Acc))};
-heartbeat(Length, [], Acc) ->
-    {incomplete, fun(Str) -> heartbeat(Length, Str, Acc) end};
 heartbeat(Length, [N|Rest], Acc) when N >= $0, N =< $9 ->
     heartbeat(Length-1, Rest, [N|Acc]).
 
