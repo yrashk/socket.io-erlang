e3c12a82ae3a9780182e849d0350fb8a17d52057
Yurii Rashkovskii <yrashk@gmail.com>
2011-03-12 13:02:42 -0800

    Adjusted tests so they are getting called after refactoring polling transports

--- test/socketio_transport_polling_tests.erl
+++ test/socketio_transport_polling_tests.erl
@@ -1,5 +1,8 @@
--module(socketio_transport_jsonp_polling_tests).
+-module(socketio_transport_polling_tests).
 -include_lib("eunit/include/eunit.hrl").
+
+transport_xhr_polling_test_() ->
+    socketio_transport_tests:transport_tests("xhr-polling").
 
 transport_jsonp_polling_test_() ->
     socketio_transport_tests:transport_tests("jsonp-polling").
--- test/socketio_transport_xhr_polling_tests.erl
+++
@@ -1,5 +0,0 @@
--module(socketio_transport_xhr_polling_tests).
--include_lib("eunit/include/eunit.hrl").
-
-transport_xhr_polling_test_() ->
-    socketio_transport_tests:transport_tests("xhr-polling").
