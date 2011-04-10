06e839f6721be71f47ca7edc453e749743f5e015
Yurii Rashkovskii <yrashk@gmail.com>
2011-04-09 19:48:34 -0700

    Added socketio_http_server behaviour

--- src/socketio_http_misultin.erl
+++ src/socketio_http_misultin.erl
@@ -1,4 +1,5 @@
 -module(socketio_http_misultin).
+-behaviour(socketio_http_server).
 -export([start_link/1, file/2, respond/2, respond/3, respond/4, parse_post/1, headers/2, chunk/2, stream/2,
          socket/1, get_headers/1]).
 
